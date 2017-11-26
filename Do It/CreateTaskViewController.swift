788947c9f2d721277eea29a642cae7e754775ce2
Sophie Amin <aminsophie@gmail.com>
2017-11-26 16:38:54 -0700

    View controller added for creating tasks, able to add tasks to the Tasks tableview

---
+++ Do It/CreateTaskViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreateTaskViewController.swift
+//  Do It
+//
+//  Created by Sophie Amin on 2017-11-26.
+//  Copyright © 2017 Sophie Amin. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+    
+
+
+
+}
---
+++ Do It/TasksViewController.swift
@@ -0,0 +1,66 @@
+//
+//  TasksViewController.swift
+//  Do It
+//
+//  Created by Sophie Amin on 2017-11-25.
+//  Copyright © 2017 Sophie Amin. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self //look to this class
+        tableView.delegate = self
+    }
+
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count //count the current number of tasks
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        }
+        else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
+
+    @IBAction func plusTapped(_ sender: Any){
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+}
+
