e422fa068ece84f99c2732e489d5e707719af173
Sophie Amin <aminsophie@gmail.com>
2017-11-30 17:29:14 -0700

    Deleting tasks

---
+++ Do It/CompleteTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompleteTaskViewController.swift
+//  Do It
+//
+//  Created by Sophie Amin on 2017-11-30.
+//  Copyright © 2017 Sophie Amin. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
